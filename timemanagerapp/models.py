13c608f309ee200a61bb18fd0b75f9c8a3ff4106
James Martinez <elloromtz@gmail.com>
2015-12-23 15:12:38 -0600

    Made model, made migrations and added urls.py to timemanagerapp dir

--- timemanagerapp/models.py
+++ timemanagerapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Entry(models.Model):
+    start = models.DateField()
+    end = models.DateField()
+    task = models.ForeignKey('Task')
+
+    class Meta:
+        ordering = ['-start']
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ['name']
